4812375bba3c98c7cf105ad4bcf13ab24929ee47
Juan Ignacio Ortiz Innocenti <60118480+juancitortiz@users.noreply.github.com>
2021-05-14 11:09:45 -0300

    Primer commitgit add .
    Agregada básicamente el tutorial de la página oficial de LWJGL, pero Gabe lo hace ligeramente distinto.

---
+++ src/main/java/mario/Main.java
@@ -0,0 +1,19 @@
+package mario;
+
+import buggyEngine.Window;
+
+/**
+ *
+ * @author El Juanelo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window window = Window.get();
+        window.run();
+    }
+    
+}
